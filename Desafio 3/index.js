3e10223fe757df84138eb39d542c0ecc2660b2a7
Gres29 <75045273+Gres29@users.noreply.github.com>
2022-09-07 16:14:04 -0300

    Desafio 3

--- Desafio 3/index.js
+++ Desafio 3/index.js
@@ -5,19 +5,19 @@ const PORT = process.env.PORT || 8080;
 const { Container } = require(`./Classes/Container`);
 const container = new Container(`./products.txt`, fs);
 
-const getAllAlbums = async (req, res) => {
+const getAllBooks = async (req, res) => {
   const books = await container.getAll();
   res.status(200).json(books);
 };
 
-const getRandomAlbum = async (req, res) => {
+const getRandomBook = async (req, res) => {
   const randomId = Math.floor(Math.random() * (3 - 1 + 1) + 1);
   const randomProduct = await container.getById(randomId);
   res.status(200).json(randomProduct);
 };
 
-app.get("/productos", getAllAlbums);
-app.get("/productoRandom", getRandomAlbum);
+app.get("/productos", getAllBooks);
+app.get("/productoRandom", getRandomBook);
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto: ${PORT}`);
